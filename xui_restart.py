e4e8bf5c2483aa1f937643ea35c88a86b625d547
hadimousavi79 <68472072+hadimousavi79@users.noreply.github.com>
2025-01-14 14:35:42 +0330

    Update xui_restart.py
    add cpu check too

--- xui_restart.py
+++ xui_restart.py
@@ -4,7 +4,6 @@ import os
 import psutil    # --> pip install psutil
 from pytz import timezone    # --> pip install pytz
 
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 file_path = os.path.join(dir_path, 'restart_log.txt')
 
@@ -13,10 +12,17 @@ t1 = datetime.datetime.now(timezone('Asia/Tehran'))
 while(True):
     time.sleep(10)
     p_mem = psutil.virtual_memory().percent
+    p_cpu = psutil.cpu_percent(interval=1)
     # print(p_mem)
     if(p_mem > 90):
         with open(file_path, 'a') as f:
             t2 = datetime.datetime.now(timezone('Asia/Tehran'))
             print(str(p_mem) + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1), file=f)
+            t1 = t2
+        os.system("xrayr restart")
+    if(p_cpu >= 80):
+        with open(file_path, 'a') as f:
+            t2 = datetime.datetime.now(timezone('Asia/Tehran'))
+            print("CPU: " + str(p_cpu) + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1), file=f)
             t1 = t2
         os.system("xrayr restart")
