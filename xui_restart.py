c04cdd59ac3481649283b3ed0d2a7faa529570de
hadimousavi79 <68472072+hadimousavi79@users.noreply.github.com>
2025-01-14 15:35:26 +0330

    Update xui_restart.py
    add print log

--- xui_restart.py
+++ xui_restart.py
@@ -9,19 +9,22 @@ file_path = os.path.join(dir_path, 'restart_log.txt')
 t1 = datetime.datetime.now()
 
 while(True):
-    time.sleep(10)
     p_mem = psutil.virtual_memory().percent
     p_cpu = psutil.cpu_percent(interval=1)
-    # print(p_mem)
+    print(f"Memory Usage: {p_mem}%, CPU Usage: {p_cpu}%")  # Print memory and CPU usage to terminal
     if(p_mem > 90):
         with open(file_path, 'a') as f:
             t2 = datetime.datetime.now()
-            print(str(p_mem) + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1), file=f)
+            log_message = f"{p_mem}%    time: {t2.strftime('%Y-%m-%d %H:%M:%S')}    delta: {t2-t1}"
+            print(log_message)  # Print log message to terminal
+            print(log_message, file=f)
             t1 = t2
         os.system("xrayr restart")
     if(p_cpu >= 80):
         with open(file_path, 'a') as f:
             t2 = datetime.datetime.now()
-            print("CPU: " + str(p_cpu) + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1), file=f)
+            log_message = f"CPU: {p_cpu}%    time: {t2.strftime('%Y-%m-%d %H:%M:%S')}    delta: {t2-t1}"
+            print(log_message)  # Print log message to terminal
+            print(log_message, file=f)
             t1 = t2
         os.system("xrayr restart")
